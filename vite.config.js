18d937cd590afddfee211b291564d30bbca31639
best2025j <audugodstime95@gmail.com>
2025-02-12 12:56:41 +0100

    Updated GitHub Pages deployment settings

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-
+  base: "/student-data/", // ✅ Set base path for GitHub Pages
   build: {
     chunkSizeWarningLimit: 1000, // ✅ Increase the warning limit
     rollupOptions: {

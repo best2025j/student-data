963b87a8d8ce2fd1a05e7a095a1a71078911541e
best2025j <audugodstime95@gmail.com>
2025-02-13 11:11:11 +0100

    changes made to vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/student-data/", // ✅ Fix for GitHub Pages
+  base:
+    process.env.NODE_ENV === "production" && process.env.VERCEL !== "1"
+      ? "/student-data/"
+      : "/", // ✅ Fix for GitHub Pages and vercel
 
   build: {
     outDir: "dist", // Make sure this matches your deployment settings
